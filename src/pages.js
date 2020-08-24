7a071b310b041123b5b24a9461729a9dc06228e9
brenoq <brenoq@gmail.com>
2020-08-23 23:30:40 -0300

    update: page study integration with database

--- src/pages.js
+++ src/pages.js
@@ -1,14 +1,47 @@
 const Database = require("./database/db.js");
 
-const { subjects, weekdays, getSubject } = require("./utils/format.js");
+const { subjects, weekdays, getSubject, convertHoursToMinutes } = require("./utils/format.js");
+
 
 function pageLanding (req, res) {
   return res.render("index.html");
 }
 
-function pageStudy (req, res) {
+async function pageStudy (req, res) {
   const filters = req.query;
-  return res.render("study.html", { proffys, filters, subjects, weekdays });
+
+  if (!filters.subject || !filters.weekday || !filters.time) {
+    return res.render("study.html", { filters, subjects, weekdays });
+  }
+
+  // Converter Horas em minutos
+  const timeToMinutes = convertHoursToMinutes(filters.time)
+
+  const query = `
+    SELECT classes.*, proffys.*
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE EXISTS (
+      SELECT class_schedule.* 
+      FROM  class_schedule 
+      WHERE class_schedule.class_id = classes.id
+      AND class_schedule.weekday = ${filters.weekday}
+      AND class_schedule.time_from <= ${timeToMinutes}
+      AND class_schedule.time_to > ${timeToMinutes}
+    )
+    AND classes.subject = '${filters.subject}'
+  `
+
+  // Caso haja erro na consulta do banco de dados
+  try {
+    const db = await Database;
+    const proffys = await db.all(query);
+
+    return res.render("study.html", { proffys, filters, subjects, weekdays });
+
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function pageGiveClasses (req, res) {
